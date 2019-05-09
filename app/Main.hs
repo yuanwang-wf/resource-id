03517dd7815bf57ae37f8668ad86f81e5418317a
Yuan Wang <yuan.wang@workiva.com>
2019-05-09 13:20:18 -0600

    minor change

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified Data.ByteString.Char8  as C
-import Lib
-import System.Environment
-import System.Exit
+import           Lib
+import           System.Environment
+import           System.Exit
 
 
 work :: String -> IO ()
@@ -13,4 +12,4 @@ work = display . decode . C.pack
 main :: IO ()
 main = do
     args <- getArgs
-    if (length args == 1) then work (head args) else die "incorrect argument"
+    if length args == 1 then work (head args) else die "incorrect argument"
